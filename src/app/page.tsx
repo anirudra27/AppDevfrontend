06cffd4201a8a61625e7ebf83c5dc84b30fbe9c1
annie-itani <annieitani77@gmail.com>
2024-05-07 18:35:23 +0545

    css and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,74 +9,70 @@ import {
 } from "@mui/material";
 import styles from "./page.module.css";
 
+const blogData = [
+  {
+    title: "My first trek to AmaYangri",
+    image: "Bhanu.jpg",
+    author: "Anguri Pandey",
+  },
+  {
+    title: "Tyson with tiger",
+    image: "Tyson.jpg",
+    author: "Anirudra Shrestha",
+  },
+  {
+    title: "Trip to Lumbini",
+    image: "Lumbini.jpg",
+    author: "Bhanuumatiii",
+  },
+  {
+    title: "Sunshine and smile",
+    image: "annie.jpg",
+    author: "Anniieeeee",
+  },
+  {
+    title: "Miss Sunshine ",
+    image: "annie2.jpg",
+    author: "Anniieeeee",
+  },
+  {
+    title: "Summer Swimming",
+    image: "swim.jpeg",
+    author: "Anniieeeee",
+  },
+];
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.popular_blogs}>
         <h3>Popular Blogs</h3>
-        <div className={styles.content}>
           <div className={styles.cardContainer}>
-            {/* First Card */}
-            <Card sx={{ maxWidth: 300, flex: 1, margin: '0 8px' }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image="Bhanu.jpg"
-                  alt="Trek"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    My first trek to AmaYangri
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    By Anguri Pandey
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-
-            {/* Second Card */}
-            <Card sx={{ maxWidth: 300, flex: 1, margin: '0 8px' }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image="Tyson.jpg" 
-                  alt="Tyson"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    Tyson with tiger
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    By Anirudra Shrestha
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-
-            {/* Third Card */}
-            <Card sx={{ maxWidth: 300, flex: 1, margin: '0 8px' }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image="Lumbini.jpg" 
-                  alt="Trip"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    Trip to Lumbini
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    By Bhanuumatiii
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+            {blogData.map((blog, index) => (
+              <Card sx={{ maxWidth: 300, flex: 1, 
+                margin: "8px 8px",  
+                backgroundColor:"#ffffff",
+                minWidth: "200px",
+              }} key={index}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="300px"
+                    image={blog.image}
+                    alt={blog.title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h6" component="div">
+                      {blog.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      By {blog.author}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ))}
           </div>
-        </div>
       </div>
     </main>
   );
